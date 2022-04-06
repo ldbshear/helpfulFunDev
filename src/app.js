fc6c326557b80d98c553adaf0682997a97553eb4
Ldbshear-jones <ldbshearjones@gmail.com>
2022-04-05 21:41:18 -0400

    portfolio update
    adding hover text feature

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-document.querySelector(".allusion").style.visibility = "hidden";
-let testDiv = document.querySelector(".test");
+document.querySelectorAll(".allusion").style.visibility = "hidden";
+// let testDiv = document.querySelectorAll(".test");
 console.log("hello it's me");
-testDiv.addEventListener("mouseover", function () {
-  document.querySelector(".allusion").style.visibility = "visible";
-});
-testDiv.addEventListener("mouseout", function () {
-  document.querySelector(".allusion").style.visibility = "hidden";
-});
+// testDiv.addEventListener("mouseover", function () {
+//   document.querySelector(".allusion").style.visibility = "visible";
+// });
+// testDiv.addEventListener("mouseout", function () {
+//   document.querySelector(".allusion").style.visibility = "hidden";
+// });
