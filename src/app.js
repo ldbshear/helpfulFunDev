271017d4e7521ec74602277ec5058c4e3c1d9e82
Ldbshear-jones <ldbshearjones@gmail.com>
2022-04-07 22:45:37 -0400

    js changes
    javascript revisions
    removed hidden content

--- src/app.js
+++ src/app.js
@@ -1,9 +1,1 @@
-document.querySelectorAll(".allusion").style.visibility = "hidden";
-// let testDiv = document.querySelectorAll(".test");
-console.log("hello it's me");
-// testDiv.addEventListener("mouseover", function () {
-//   document.querySelector(".allusion").style.visibility = "visible";
-// });
-// testDiv.addEventListener("mouseout", function () {
-//   document.querySelector(".allusion").style.visibility = "hidden";
-// });
+let showMe = classlist.toggle("hide");
