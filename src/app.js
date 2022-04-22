3b8bce59c0cff5a2208bbff41701952c73cb31e8
Ldbshear-jones <ldbshearjones@gmail.com>
2022-04-21 20:55:53 -0400

    portfolio revisions
    added optimized imgs
    added js for user interactions

--- src/app.js
+++ src/app.js
@@ -9,3 +9,40 @@ function changeNavColor(e) {
 }
 
 document.addEventListener("scroll", changeNavColor);
+
+// function showImg(e) {
+//   console.log("helpful");
+
+//   showHelpful.style.opacity = "1";
+//   showHelpful.style.transition = "2s ease-in-out";
+// }
+
+// function hideImg(e) {}
+
+let triggerHelpful = document.getElementById("helpful");
+//
+// triggerHelpful.addEventListener("mouseout", hideImg);
+
+let hideNShow = {
+  show: function (e) {
+    e.style.opacity = "1";
+    // e.style.transform = "translateX(100px)";
+    e.style.transform = "scale(1.2)";
+    e.style.transition = "1.3s ease-in-out";
+  },
+  hide: function (e) {
+    e.style.opacity = "0";
+    e.style.transform = "scale(1)";
+    e.style.transition = ".8s ease-in-out";
+  },
+};
+triggerHelpful.addEventListener("mouseenter", function () {
+  let showHelpful = document.querySelector(".imgContainer");
+  hideNShow.show(showHelpful);
+});
+
+triggerHelpful.addEventListener("mouseout", function () {
+  let showHelpful = document.querySelector(".imgContainer");
+  hideNShow.hide(showHelpful);
+});
+console.log(hideNShow);
